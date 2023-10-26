d180aa27c667799de6c71e3b5acdcaff7c527e5c
pawan <pawandaathakuni@gmail.com>
2023-10-26 22:09:20 +0530

    commit

--- src/component/Simmer.js
+++ src/component/Simmer.js
@@ -1,23 +1,25 @@
 import React from 'react'
 
-
 function Simmer() {
   return (
-    <div className='simmer-cont'>
-      <div className='simmer'></div>
-      <div className='simmer'></div>
-      <div className='simmer'></div>
-      <div className='simmer'></div>
-      <div className='simmer'></div>
-      <div className='simmer'></div>
-      <div className='simmer'></div>
-      <div className='simmer'></div>
-      <div className='simmer'></div>
-
+    <div className='simmer-co flex flex-wrap space-x-5 ml-28 space-y-3 rounded-lg'>
+    <div className='simmer-cont w-80 h-80 bg-gray-200 rounded-lg'></div>
+    <div className='simmer-cont w-80 h-80 bg-gray-200 rounded-lg'></div>
+    <div className='simmer-cont w-80 h-80 bg-gray-200 rounded-lg'></div>
+    <div className='simmer-cont w-80 h-80 bg-gray-200 rounded-lg'></div>
+    <div className='simmer-cont w-80 h-80 bg-gray-200 rounded-lg'></div>
+    <div className='simmer-cont w-80 h-80 bg-gray-200 rounded-lg'></div>
+    <div className='simmer-cont w-80 h-80 bg-gray-200 rounded-lg'></div>
+    <div className='simmer-cont w-80 h-80 bg-gray-200 rounded-lg'></div>
+    <div className='simmer-cont w-80 h-80 bg-gray-200 rounded-lg'></div>
 
     
+
+    
+
     
     </div>
+
   )
 }
 
